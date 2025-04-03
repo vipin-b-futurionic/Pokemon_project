4568722fe6757bbb7272836d096acde445fc1f44
vipin-b-futurionic <vipin.bhaskar@futurionic.com>
2025-04-03 16:29:51 +0530

    Pokemon image fetching

---
+++ vipin_pokemon_react/src/PokemonCards.jsx
@@ -0,0 +1,20 @@
+export const PokemonCards = ({pokemonData}) => {
+    return <li className = "pokemon-card">
+        <figure>
+            <img 
+            src={pokemonData.sprites.other.dream_world.front_default} 
+            alt={pokemonData.name}
+            className="pokemon-image"
+             />
+        </figure>
+        <h1 className="pokemon-name">{pokemonData.name}</h1>
+        <div className="pokemon-info pokemon-highlight">
+            <p>
+                {
+                    pokemonData.types.map((curType) =>curType.type.name).join(", ")
+                }
+            </p>
+        </div>
+        </li>
+
+}
